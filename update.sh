01e19aaa1c204c2b625bfd8a358acf3e14527dfe
Jaehwa Han <drh@easi6.com>
2017-04-21 11:38:20 +0800

    add china mirror

--- update.sh
+++ update.sh
@@ -14,11 +14,22 @@ do
   rm -rf "$version"
   cp -R base "$version"
 
+  if [ "$version" = "edge" ];
+  then
+    tag=$version
+  else
+    tag="v$version"
+  fi
+
   cat > "$version/Dockerfile" <<-END
 # Generated automatically by update.sh
 # Do no edit this file
 
 FROM alpine:$version
+
+RUN echo $"http://mirrors.ustc.edu.cn/alpine/$tag/main/\n\\
+  http://mirrors.ustc.edu.cn/alpine/$tag/community\n"\\
+  > /etc/apk/repositories
 
 RUN apk add --no-cache bash tzdata
 
